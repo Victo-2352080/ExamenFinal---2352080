cfb674da5dd3d49269d1b3de153fe99f05c8c31f
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2025-05-08 08:37:36 -0400

    separationInterface

--- service.js
+++ service.js
@@ -4,10 +4,7 @@ import router from './Src/routes/api.route.js';
 const app = express();
 
 app.use(express.json());
-
-
 app.use('/api', router);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
